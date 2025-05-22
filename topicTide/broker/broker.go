3f91435502b4e03e137a5691ee97fd3709c8e6b1
PBLT-208 <kunal180geu@gmail.com>
2025-05-23 00:12:43 +0530

    Try to Deploy

--- topicTide/broker/broker.go
+++ topicTide/broker/broker.go
@@ -12,7 +12,7 @@ import (
 	"regexp"
 	"strings"
 	"topicTide/communication_protocol"
-
+	
 	"github.com/gorilla/websocket"
 )
 
--- topicTide/main.go
+++ topicTide/main.go
@@ -8,9 +8,15 @@ import (
 )
 
 func main() {
+	// Serve static files for frontend
+	http.Handle("/producer_ui/", http.StripPrefix("/producer_ui/", http.FileServer(http.Dir("./producer_frontend"))))
+	http.Handle("/consumer_ui/", http.StripPrefix("/consumer_ui/", http.FileServer(http.Dir("./consumer_frontend"))))
+
+	// API endpoints
 	http.HandleFunc("/producer", broker.HandleProducer)
 	http.HandleFunc("/consumer", broker.HandleConsumer)
 	http.HandleFunc("/topics", broker.ListTopics)
+
 	fmt.Println("Broker running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
